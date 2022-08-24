3dfd8ddeeac3bf35edfd46326c642c313d3e24de
Uriel Ventura <urielventura000@gmail.com>
2022-08-24 18:46:13 -0400

    Add board

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,48 @@
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import { Layout } from "../components/layouts";
 import styles from "../styles/Home.module.css";
 
 const HomePage: NextPage = () => {
   return (
-    <Layout title="Home Page">
-      <Typography variant="h1" color="primary">
-        Hola Mundo
-      </Typography>
+    <Layout title=" Home - Open Jira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card
+            sx={{
+              height: "calc(100vh - 100px)",
+            }}
+          >
+            <CardHeader title="Pendings" />
+            <CardContent>
+              {/*TODO:
+               * Add a new Entrie
+               *Entries list
+               */}
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card
+            sx={{
+              height: "calc(100vh - 100px)",
+            }}
+          >
+            <CardHeader title="In Progress" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card
+            sx={{
+              height: "calc(100vh - 100px)",
+            }}
+          >
+            <CardHeader title="Done" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
