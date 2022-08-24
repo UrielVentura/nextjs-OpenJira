fa9b9808995e427b9e6fe09877090880514c2073
Uriel Ventura <urielventura000@gmail.com>
2022-08-24 17:03:49 -0400

    Add Material UI, AppBar, SideBar and ContextApi

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,17 @@ import { Typography } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { Layout } from "../components/layouts";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const HomePage: NextPage = () => {
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <Layout title="Home Page">
+      <Typography variant="h1" color="primary">
+        Hola Mundo
+      </Typography>
+    </Layout>
   );
 };
 
-export default Home;
+export default HomePage;
